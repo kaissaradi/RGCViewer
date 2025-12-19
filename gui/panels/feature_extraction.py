1eb55e5ad2f6e4bc727578cd09cd5a9ecfb02d82
kaissaradi <kais.saradi@gmail.com>
2025-12-18 19:35:36 -0800

    sync changes between kais-huakun

--- gui/panels/feature_extraction.py
+++ gui/panels/feature_extraction.py
@@ -58,14 +58,13 @@ class FeatureExtractionWindow(QDialog):
         pca = PCA(n_components=3)
         pca_scores = pca.fit_transform(self.temporal_traces)
         return pca_scores
-    
+
     def draw_subplots(self, subplot_idx):
         if subplot_idx == 0:
             ax = self.axes[0][0]
             ax.scatter(self.pca_scores[:, 0], self.pca_scores[:, 1])
+            ax.set_title("PCA: PC1 vs PC2")
             self.canvas.draw()
-            
-            selected_ids = []
 
             def onselect(eclick, erelease):
                 x1, y1 = eclick.xdata, eclick.ydata
@@ -74,15 +73,22 @@ class FeatureExtractionWindow(QDialog):
                 xmin, xmax = sorted([x1, x2])
                 ymin, ymax = sorted([y1, y2])
 
-                flag_ROI = ((self.pca_scores[:, 0] >= xmin) & 
-                            (self.pca_scores[:, 0] <= xmax) & 
+                flag_ROI = ((self.pca_scores[:, 0] >= xmin) &
+                            (self.pca_scores[:, 0] <= xmax) &
                             (self.pca_scores[:, 1] >= ymin) &
                             (self.pca_scores[:, 1] <= ymax))
 
-                sel_ids = [cid for cid, flag in zip(self.cluster_ids, flag_ROI) if flag]
-                selected_ids.clear()
-                selected_ids.extend(sel_ids)
-                logger.debug('Selected cluster IDs: %s', sel_ids)
+                selected_ids = [cid for cid, flag in zip(self.cluster_ids, flag_ROI) if flag]
+
+                # Trigger menu immediately when selection is made
+                if selected_ids:
+                    menu = QMenu(self)
+                    create_action = menu.addAction(f"Create Group from {len(selected_ids)} clusters")
+                    action = menu.exec_(QCursor.pos())
+                    if action == create_action:
+                        self.create_new_class(selected_ids)
+
+                logger.debug('Selected cluster IDs: %s', selected_ids)
 
             self.selector = RectangleSelector(ax, onselect,
                             useblit=False,
@@ -92,33 +98,45 @@ class FeatureExtractionWindow(QDialog):
                             button=[1],
                             interactive=True)
 
+        elif subplot_idx == 1:
+            ax = self.axes[0][1]
+            ax.scatter(self.pca_scores[:, 1], self.pca_scores[:, 2])
+            ax.set_title("PCA: PC2 vs PC3")
+            self.canvas.draw()
+
+            def onselect(eclick, erelease):
+                x1, y1 = eclick.xdata, eclick.ydata
+                x2, y2 = erelease.xdata, erelease.ydata
+
+                xmin, xmax = sorted([x1, x2])
+                ymin, ymax = sorted([y1, y2])
+
+                flag_ROI = ((self.pca_scores[:, 1] >= xmin) &
+                            (self.pca_scores[:, 1] <= xmax) &
+                            (self.pca_scores[:, 2] >= ymin) &
+                            (self.pca_scores[:, 2] <= ymax))
+
+                selected_ids = [cid for cid, flag in zip(self.cluster_ids, flag_ROI) if flag]
+
+                # Trigger menu immediately when selection is made
+                if selected_ids:
+                    menu = QMenu(self)
+                    create_action = menu.addAction(f"Create Group from {len(selected_ids)} clusters")
+                    action = menu.exec_(QCursor.pos())
+                    if action == create_action:
+                        self.create_new_class(selected_ids)
+
+                logger.debug('Selected cluster IDs: %s', selected_ids)
+
+            self.selector2 = RectangleSelector(ax, onselect,
+                            useblit=False,
+                            minspanx=0.01,
+                            minspany=0.01,
+                            spancoords='data',
+                            button=[1],
+                            interactive=True)
 
 
-    def _on_select_subplot(self, eclick, erelease, subplot_idx, scatter_vals):
-        x1, y1 = eclick.xdata, eclick.ydata
-        x2, y2 = erelease.xdata, erelease.ydata
-
-        xmin, xmax = sorted([x1, x2])
-        ymin, ymax = sorted([y1, y2])
-
-        flag_ROI = ((scatter_vals[:, 0] >= xmin) & 
-                    (scatter_vals[:, 0] <= xmax) & 
-                    (scatter_vals[:, 1] >= ymin) &
-                    (scatter_vals[:, 1] <= ymax))
-
-        selected_ids = [cid for cid, flag in zip(self.cluster_ids, flag_ROI) if flag]
-
-        menu = QMenu(self)
-        add_new_class_action = menu.addAction("New class")
-        action = menu.exec_(QCursor.pos())
-        if action == add_new_class_action:
-            selector = self.selectors[subplot_idx]
-            for artist in selector.artists:
-                artist.set_visible(False)
-            self.canvas.draw_idle()
-            print("Selected cluster IDs:", selected_ids)
-            self.create_new_class(selected_ids)
-    
     def create_new_class(self, selected_ids):
         if self.main_window.data_manager is None:
             return
@@ -143,47 +161,47 @@ class FeatureExtractionWindow(QDialog):
             group_item = QStandardItem(label)
             # group_item.setEditable(False)
             group_item.setDropEnabled(True)  # Can drop cells into it
-            
+
             # Style group items differently from cells
             font = group_item.font()
             font.setBold(True)
             group_item.setFont(font)
-            
+
             # Set different background color for groups
             group_item.setBackground(QColor('#3C3C3C'))  # Dark gray background for groups
-            
+
             # Add folder icon for groups
             group_item.setIcon(self.main_window.style().standardIcon(QStyle.StandardPixmap.SP_DirIcon))
-            
+
             groups[label] = group_item
             model.appendRow(group_item)
-            
+
         # Add each cluster as a child item to its group
         for _, row in df.iterrows():
             cluster_id = row['cluster_id']
             label = row['KSLabel']
-            
+
             # The text displayed will be e.g., "Cluster 123 (n=456 spikes)"
             item_text = f"Cluster {cluster_id} (n={row['n_spikes']})"
             cell_item = QStandardItem(item_text)
             cell_item.setEditable(False)
-            
+
             # Add a special icon or style for cells to distinguish them
             font = cell_item.font()
             font.setItalic(False)
             cell_item.setFont(font)
-            
+
             # Add file icon for cells
             cell_item.setIcon(self.main_window.style().standardIcon(QStyle.StandardPixmap.SP_FileIcon))
-            
+
             # IMPORTANT: Store the actual cluster ID in the item's data role.
             # This is how we'll retrieve it when the item is clicked.
             cell_item.setData(cluster_id, Qt.ItemDataRole.UserRole)
-            
+
             # Prevent dropping items onto cells
             cell_item.setDropEnabled(False)
-            
+
             groups[label].appendRow(cell_item)
-            
+
         self.main_window.setup_tree_model(model)
         self.main_window.tree_view.expandAll()
